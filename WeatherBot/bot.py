f39b7ccb8e6ee6b9b75112d15d39a8000aeb1075
Vlad Scepetov <vlad.scepetov@outlook.com>
2024-05-20 15:20:01 +0300

    Added requirements and fixed errors.

--- WeatherBot/bot.py
+++ WeatherBot/bot.py
@@ -5,9 +5,11 @@ import json
 bot = telebot.TeleBot('7143647141:AAFYmigpj88MKWqqd7kXFQT7G_McQsT-HoA')
 API = '9b6486149040c51e7fb61b95f0557c08'
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Enter City Name.')
+
 
 @bot.message_handler(content_types=['text'])
 def get_weather(message):
@@ -18,9 +20,9 @@ def get_weather(message):
         temp = data["main"]["temp"]
         for weather in data["weather"]:
             description = weather["main"]
-        bot.send_message(message.chat.id,f'Weather in {message.text} Now is {temp} °C {description}')
+        bot.send_message(message.chat.id, f'Weather in {message.text} Now is {temp} °C {description}')
     else:
-        bot.send_message(message.chat.id,f'Non existent City')
+        bot.send_message(message.chat.id, f'Non existent City')
 
 
 bot.polling(none_stop=True)
